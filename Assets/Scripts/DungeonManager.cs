18dbb551c9885969b2dc1a96ce1777dda11153c5
rhkrwnstjd19 <78295811+rhkrwnstjd19@users.noreply.github.com>
2024-09-10 23:36:30 +0900

    Add : StaticMapLoader
    오픈소스 테스트용 맵

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
+//아직 사용 미정.
 namespace Game
 {
     public class DungeonManager : Singleton<DungeonManager>
