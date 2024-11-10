e37b37e8d1d1feec15f57dd517f5ce6351681e9f
ovthemoon <seungmoonchoi71@gmail.com>
2024-11-10 21:46:30 +0900

    Add : UI Effect

--- Assets/Scripts/UI/Adventure/AdventureDetailPanel.cs
+++ Assets/Scripts/UI/Adventure/AdventureDetailPanel.cs
@@ -3,12 +3,14 @@ using TMPro;
 using Cinemachine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Collections.Generic;
 
 public class AdventureDetailPanel : MonoBehaviour
 {
     [Header("UI Component")]
     public TMP_Text dungeonName;
     public TMP_Text dungeonLevel;
+    public TMP_Text characterName;
     public Transform buttonPanel;
     public Button CloseButton;
     public PetButton petButton;
@@ -17,7 +19,8 @@ public class AdventureDetailPanel : MonoBehaviour
     public float animationSpeed = 1.5f;
     private Vector2 originalPosition;
     private Vector2 closePosition;
-
+    private List<PetButton> petButtons=new();
+    private int currentSelectedIndex=-1;
     private void Awake()
     {
         // 패널의 초기 위치를 저장합니다.
@@ -29,11 +32,12 @@ public class AdventureDetailPanel : MonoBehaviour
     public void Init(AdvDungeon dungeon)
     {
         gameObject.SetActive(true);
+        //UI Setting
         dungeonName.text = dungeon.dungeonName;
         dungeonLevel.text = dungeon.dungeonLevel.ToString();
+        characterName.text="**캐릭터를 선택하세요**";
         dungeon.targetAheadCamera.Priority = 30;
-
-        GetPetList();
+        
         CloseButton.onClick.AddListener(() =>
         {
             dungeon.targetAheadCamera.Priority = 10;
@@ -43,19 +47,21 @@ public class AdventureDetailPanel : MonoBehaviour
         {
             StartDungeon();
         });
+        //내부 로직
+        GetPetList();
 
         OpenAnimation();
     }
 
     public void OpenAnimation()
     {
-        // 패널이 부드럽게 올라오는 애니메이션
+        // 패널 애니메이션(Linear로 올라옴)
         transform.DOLocalMoveY(originalPosition.y, animationSpeed)
         .SetEase(Ease.Linear);
     }
     public void CloseAnimation()
     {
-        // 패널이 부드럽게 올라오는 애니메이션
+        // 패널 애니메이션(Linear로 내려감)
         transform.DOLocalMoveY(closePosition.y, animationSpeed)
         .SetEase(Ease.Linear)
         .OnComplete(() => gameObject.SetActive(false));
@@ -67,7 +73,15 @@ public class AdventureDetailPanel : MonoBehaviour
         // return List<Pet>으로 변경
         // 받아온 Pet 데이터 기반 PetButton Init
     }
-
+    private void SelectPet(int buttonNumber){
+        if(currentSelectedIndex==-1){
+            
+        }else{
+            petButtons[currentSelectedIndex].Deselect();
+            currentSelectedIndex=buttonNumber;
+            petButtons[currentSelectedIndex].WhenSelect();
+        }
+    }
     public void StartDungeon()
     {
         // ***TODO***
--- Assets/Scripts/UI/Adventure/PetButton.cs
+++ Assets/Scripts/UI/Adventure/PetButton.cs
@@ -1,9 +1,19 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PetButton : MonoBehaviour{
     public Image image;
-    public void Init(Sprite sprite){
+    public int buttonNumber{get;private set;}
+    public bool isSelected{get;private set;}=false;
+    public void Init(Sprite sprite,int number){
+        buttonNumber=number;
         image.sprite=sprite;
+    }
+    public void WhenSelect(){
+        isSelected=true;
+    }
+    public void Deselect(){
+        isSelected=false;
     }
 }
